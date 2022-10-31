043912d4431b729d5296ffa31f616ff7566b1456
nonso7 <felixmedia78@gmail.com>
2022-10-31 10:24:39 -0700

    Revert "build project"
    This reverts commit 24d89ee8865ac4db02cb3d3c1dbb3478ffe3881e.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: path.join(__dirname, "src"),
-  build: {
-    outDir: path.join(__dirname, "docs"),
-  },
+  plugins: [react()]
 })
-
-
